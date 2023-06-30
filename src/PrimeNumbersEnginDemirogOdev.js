27092baa5a72c242073d667dce04d70fac289393
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-06-30 22:15:38 +0300

    UpdatedJavaScriptFilesEnginDemirog1

--- src/PrimeNumbersEnginDemirogOdev.js
+++ src/PrimeNumbersEnginDemirogOdev.js
@@ -1,14 +1,12 @@
 
-function primeNumber(isPrime){
-    for(let i = 2; i <= 1000; i++)
-        if(isPrime[i] % i != 0){
-            console.log(isPrime[i])
-        }
+function findPrime(...primeNumber){
+    for (let i = 0; i < primeNumber.length; i++) {
+        if(primeNumber[i] % i != 0){
+            console.log(primeNumber[i])
+        } 
     }
-primeNumber(1000)
-
-
-
+}
+findPrime(2, 5, 8, 21, 13, 25, 31, 17, 16, 103)
 
 
 
--- src/mapFilterReduce.js
+++ src/mapFilterReduce.js
@@ -1,5 +1,38 @@
+let cart = [
+    {id : 1, productName : "Phone", quantity : 3, unitPrice : 4000},
+    {id : 2, productName : "Glass", quantity : 5, unitPrice : 10},
+    {id : 3, productName : "Pencil", quantity : 4, unitPrice : 20},
+    {id : 4, productName : "Phone Charge", quantity : 2, unitPrice : 100},
+    {id : 5, productName : "Book", quantity : 7, unitPrice : 50},
+    {id : 6, productName : "Notebook", quantity : 1, unitPrice : 40}
+
+]
+//cart.map(product => console.log(product.productName))
+//cart.map(product => console.log(product))
+console.log("<ul>")
+cart.map(product => {
+    console.log("<li>" + product.productName + " : " + product.unitPrice * product.quantity + "</li>")
+})
+console.log("</ul>")
+
+//Single Page Application(SPA) : View, React ve Angular ile geliştirdiğimiz uygulamalar
+
+//cart.push({id : 7, productName : "Headset", quantity : 6, unitPrice : 300})
 
 
+function addToCart(basket) {
+    basket.push({id : 7, productName : "Headset", quantity : 6, unitPrice : 300})
+}
 
+addToCart(cart)
 
+console.log(cart)
 
+let number = 10
+
+function sumOfNumbers(number1) {
+    number1 += 1
+}
+
+sumOfNumbers(number)
+console.log(number)
