ca16dccaa9a88796fe1a152b7abb38cf5c4fcde2
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2023-07-05 09:37:03 +0300

    UpdatedJavaScriptEnginDemirog

--- src/crossCuttingConcernsMyFile/logger.js
+++ src/crossCuttingConcernsMyFile/logger.js
@@ -1,19 +1,19 @@
-export class BaseLogger{
-    log(data){
+export class BaseLogger {
+    log(data) {
         console.log("Default logger : " + data)
     }
-    
-}
-export class ElasticLogger extends BaseLogger{
-        log(data){
-            console.log("Elastic logger : " + data)
-        }
 
 }
-export class MongoLogger extends BaseLogger{
-        log(data){
-            console.log("Mongo Logger : " + data)
-        }
+export class ElasticLogger extends BaseLogger {
+    log(data) {
+        console.log("Elastic logger : " + data)
+    }
+
+}
+export class MongoLogger extends BaseLogger {
+    log(data) {
+        console.log("Mongo Logger : " + data)
+    }
 }
 
 
